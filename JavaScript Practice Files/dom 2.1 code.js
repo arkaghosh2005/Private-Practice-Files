a192be066ba9947d97353bf8ec6b9c426752cfac
Arka Ghosh <arka.ghosh20057290@gmail.com>
2024-06-12 19:19:53 +0530

    practice questions dom 2.2

--- JavaScript Practice Files/dom 2.1 code.js
+++ JavaScript Practice Files/dom 2.1 code.js
@@ -1,8 +1,3 @@
-let div1 = document.querySelector("div");
-console.log(div1);
-
-
-
 //To get the attribute value
 let para1 = document.querySelector("p");
 console.log(para1.getAttribute("class"));
@@ -27,8 +22,19 @@ console.log(newbtn);
 
 //Step 2: Addition in the Webpage
 //Opt 1: <node>.append(<item>) --- Adds at the end of node (inside)
-//Opt 1: <node>.prepend(<item>) --- Adds at the start of node (inside)
-//Opt 1: <node>.before(<item>) --- Adds before the node (outside)
-//Opt 1: <node>.after(<item>) --- Adds after the node (outside)
+//Opt 2: <node>.prepend(<item>) --- Adds at the start of node (inside)
+//Opt 3: <node>.before(<item>) --- Adds before the node (outside)
+//Opt 4: <node>.after(<item>) --- Adds after the node (outside)
+//opt 5: <node>.appendChild(<item>) --- Same as Append but used in lists
 let addbtn = document.getElementById("box2");
 addbtn.append(newbtn);
+
+//Adding an element based on the upper 2 steps
+let newhead = document.createElement("h1");
+newhead.innerHTML="<i>Hi I am new to JavaScript</i>";
+document.querySelector("body").prepend(newhead);
+
+
+//Removal or Deletion of Elements
+let rmp = document.querySelector("p");
+rmp.remove()
