c3dfdb1b61516ea2eec511e077dd8bd56e9759e7
Arka Ghosh <arka.ghosh20057290@gmail.com>
2024-06-16 13:48:03 +0530

    Delete JavaScript Practice Files directory

--- JavaScript Practice Files/dom 1.2 practice.js
+++
@@ -1,4 +0,0 @@
-let heading = document.querySelector("h2");
-console.dir(h2.innerText);
-//Change to be done
-h2.innerText = "My name is " + h2.innerText;
--- JavaScript Practice Files/username generator.js
+++
@@ -1,5 +0,0 @@
-let str = prompt("Enter your full name (without spaces):");
-let low = str.toLowerCase()
-let len = str.length
-let username = '@'+low+len
-console.log(username)
